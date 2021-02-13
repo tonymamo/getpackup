e4b780e9dab88fe21f699269d8503d771dec1e0e
Kevin Shreve <kshreve@gmail.com>
2021-02-13 14:45:43 -0500

    inline add input textbox

--- src/components/PackingListAddItem.tsx
+++ src/components/PackingListAddItem.tsx
@@ -28,74 +28,55 @@ const PackingListItemWrapper = styled.li`
 `;
 
 const PackingListAddItem: FunctionComponent<PackingListItemProps> = ({ tripId, categoryName }) => {
-  const [showAddForm, setShowAddForm] = useState(false);
   const firebase = useFirebase();
   const dispatch = useDispatch();
-  const handleKeyDown = (ev) => {
-    if (ev.keyCode === 13) {
-      setShowAddForm(true);
-    }
-  };
 
   return (
     <PackingListItemWrapper>
-      {showAddForm && (
-        <Formik
-          validateOnMount
-          initialValues={{}}
-          onSubmit={async (values, { resetForm }) => {
-            try {
-              await firebase
-                .firestore()
-                .collection('trips')
-                .doc(tripId)
-                .collection('packing-list')
-                .add({ ...values, quantity: 1, category: categoryName });
+      <Formik
+        validateOnMount
+        initialValues={{}}
+        onSubmit={async (values, { resetForm }) => {
+          try {
+            await firebase
+              .firestore()
+              .collection('trips')
+              .doc(tripId)
+              .collection('packing-list')
+              .add({ ...values, quantity: 1, category: categoryName });
 
-              resetForm({});
-            } catch (err) {
-              await dispatch(
-                addAlert({
-                  type: 'danger',
-                  message: err.message,
-                })
-              );
-            }
-          }}
-        >
-          {({ handleSubmit }) => (
-            <Form onSubmit={handleSubmit}>
-              <FlexContainer justifyContent="space-between">
-                <FlexContainer justifyContent="space-between" flexDirection="column">
-                  <Field
-                    as={Input}
-                    type="text"
-                    name="name"
-                    label="Name"
-                    validate={requiredField}
-                    required
-                  />
-                </FlexContainer>
-                <IconWrapper onClick={handleSubmit}>
-                  <FaPlus />
-                </IconWrapper>
+            resetForm({});
+          } catch (err) {
+            await dispatch(
+              addAlert({
+                type: 'danger',
+                message: err.message,
+              })
+            );
+          }
+        }}
+      >
+        {({ handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <FlexContainer justifyContent="space-between">
+              <FlexContainer justifyContent="space-between" flexDirection="column">
+                <Field
+                  as={Input}
+                  type="text"
+                  name="name"
+                  label="Add Item"
+                  validate={requiredField}
+                  hiddenLabel
+                  required
+                />
               </FlexContainer>
-            </Form>
-          )}
-        </Formik>
-      )}
-      {!showAddForm && (
-        <div
-          role="button"
-          tabIndex={0}
-          onKeyDown={handleKeyDown}
-          onClick={() => setShowAddForm(true)}
-        >
-          <p>
-            <FaPlus /> Add Item...
-          </p>
-        </div>
-      )}
+              <IconWrapper onClick={handleSubmit}>
+                <FaPlus />
+              </IconWrapper>
+            </FlexContainer>
+          </Form>
+        )}
+      </Formik>
     </PackingListItemWrapper>
   );
 };
