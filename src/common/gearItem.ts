11c7ca575a414e99b20b2e0ba7199278d5e7a19a
Adam Laycock | Versett <adam.laycock@versett.com>
2021-04-18 12:14:58 -0600

    Update gear item type

--- src/common/gearItem.ts
+++ src/common/gearItem.ts
@@ -4,5 +4,35 @@ export type GearItem = {
   category: string;
   lastEditedBy?: string;
   essential: boolean;
-  [key: string]: boolean | string | undefined; // all the rest... TODO: type rest of the fields out
+
+  // All the activity types
+  airplane: boolean;
+  backcountryThreeSeason: boolean;
+  backcountryWinter: boolean;
+  basicHut: boolean;
+  biking: boolean;
+  bivy: boolean;
+  boat: boolean;
+  bouldering: boolean;
+  bus: boolean;
+  car: boolean;
+  carCamp: boolean;
+  carCamping: boolean;
+  dayTrip: boolean;
+  hiking: boolean;
+  hostel: boolean;
+  hotel: boolean;
+  iceClimbing: boolean;
+  motorcycle: boolean;
+  multidayTrip: boolean;
+  paddling: boolean;
+  resortSkiing: boolean;
+  servicedHut: boolean;
+  skiTouring: boolean;
+  snowboarding: boolean;
+  snowshoeing: boolean;
+  sportClimbing: boolean;
+  tent: boolean;
+  tradClimbing: boolean;
+  train: boolean;
 };
