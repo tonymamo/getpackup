14f5dcf1e300ff86cb855ea84195cf9a52fa3b38
Tony Mamo <tmamo13991@gmail.com>
2025-12-31 23:19:10 -0500

    potential fix for GETPACKUP-GM

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,46 +1,19 @@
-import { TripMemberStatus, TripType } from '@common/trip';
-import {
-  Avatar,
-  Button,
-  Column,
-  FlexContainer,
-  Heading,
-  HorizontalRule,
-  NotificationDot,
-  PageContainer,
-  Row,
-} from '@components';
-import GearClosetIcon from '@images/gearClosetIcon';
-import { useLocation } from '@reach/router';
-import { RootState } from '@redux/ducks';
-import { brandPrimary, brandSecondary, brandSecondaryHover, textColor, white } from '@styles/color';
-import { zIndexSmallScreenFooter } from '@styles/layers';
-import { baseBorderStyle, visuallyHiddenStyle } from '@styles/mixins';
-import { baseSpacer, halfSpacer, quadrupleSpacer } from '@styles/size';
-import { fontSizeH3, fontSizeSmall } from '@styles/typography';
-import { ThemeContext } from '@utils/ThemeContext';
+import { Badges, Button, Column, FlexContainer, Heading, PageContainer, Row } from '@components';
+import mountainScene from '@images/mountain-scene.png';
+import { brandSecondary, white } from '@styles/color';
+import { visuallyHiddenStyle } from '@styles/mixins';
+import { baseSpacer, halfSpacer, tripleSpacer } from '@styles/size';
 import trackEvent from '@utils/trackEvent';
-import useWindowSize from '@utils/useWindowSize';
 import { Link } from 'gatsby';
 import React from 'react';
-import {
-  FaCalendar,
-  FaFacebook,
-  FaInstagram,
-  FaShoppingCart,
-  FaTwitter,
-  FaUserLock,
-} from 'react-icons/fa';
-import { useSelector } from 'react-redux';
-import { isLoaded } from 'react-redux-firebase';
+import { FaFacebook, FaInstagram, FaReddit } from 'react-icons/fa';
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
-  background-color: ${brandSecondary};
-  // background-color: var(--color-secondary);
   color: ${white};
-  padding: ${quadrupleSpacer} 0;
-  font-size: ${fontSizeSmall};
+  width: 100%;
+  position: relative;
+  z-index: 1;
   & a {
     color: ${white};
     opacity: 0.8;
@@ -61,174 +34,40 @@ const HiddenText = styled.span`
   ${visuallyHiddenStyle};
 `;
 
-const SignupFormWrapper = styled.div`
-  padding: ${quadrupleSpacer} 0;
-  background-color: ${brandSecondaryHover};
-  text-align: center;
-`;
-
-const BottomNav = styled.nav`
-  position: fixed;
-  z-index: ${zIndexSmallScreenFooter};
-  bottom: 0;
-  min-height: calc(${quadrupleSpacer} + 1px); /* min height plus 1px border top */
-  left: 0;
-  right: 0;
-  display: flex;
-  background-color: ${white};
-  border-top: ${baseBorderStyle};
-  padding-bottom: env(safe-area-inset-bottom);
-
-  & a {
-    border-top: 2px solid transparent;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex: 1;
-    height: ${quadrupleSpacer};
-    color: ${textColor};
-    // color: var(--color-text);
-    transition: all 0.2s ease-in-out;
-    position: relative;
-    font-size: ${fontSizeH3};
-  }
-
-  & a:focus {
-    outline: none;
-  }
-
-  & a.active {
-    border-top-color: ${brandPrimary};
-    color: ${brandPrimary};
-  }
+const Section = styled.section<{ background?: string }>`
+  background-color: ${(props) => props.background || 'transparent'};
+  position: relative;
+  min-height: 100vh;
 `;
 
 const Footer = () => {
-  const auth = useSelector((state: RootState) => state.firebase.auth);
-  const profile = useSelector((state: RootState) => state.firebase.profile);
-  const trips: Array<TripType> = useSelector((state: RootState) => state.firestore.ordered.trips);
-  const loggedInUser = auth && auth.isLoaded && !auth.isEmpty;
-  const size = useWindowSize();
-  const location = useLocation();
-
-  const { colorMode, setColorMode } = React.useContext(ThemeContext);
-
-  const nonArchivedTrips: TripType[] =
-    isLoaded(trips) && Array.isArray(trips) && trips && trips.length > 0
-      ? trips.filter((trip: TripType) => trip.archived !== true)
-      : [];
-
-  const pendingTrips = nonArchivedTrips.filter(
-    (trip) =>
-      trip.tripMembers &&
-      trip.tripMembers[auth.uid] &&
-      trip.tripMembers[auth.uid].status === TripMemberStatus.Pending
-  );
-
-  const isInOnboardingFlow = location.pathname.includes('onboarding');
-
-  const isPartiallyActive = ({ isPartiallyCurrent }: { isPartiallyCurrent: boolean }) => {
-    return isPartiallyCurrent ? { className: 'active' } : {};
-  };
-
-  if (!auth.isLoaded) {
-    return null;
-  }
-
   return (
-    <>
-      {!loggedInUser && (
-        <>
-          <SignupFormWrapper id="signup">
-            <PageContainer>
-              <Heading as="h1" inverse align="center">
-                Plan your first trip today
-              </Heading>
+    <Section background={brandSecondary}>
+      <FlexContainer
+        justifyContent="space-evenly"
+        flexDirection="column"
+        height="100vh"
+        alignItems="center"
+      >
+        <PageContainer>
+          <div style={{ textAlign: 'center' }}>
+            <Heading as="h1" inverse align="center">
+              Plan your first trip today
+            </Heading>
 
-              <Button type="link" to="https://packupapp.com">
-                Get Started
-              </Button>
-            </PageContainer>
-          </SignupFormWrapper>
-
+            <Button type="link" to="https://packupapp.com/signup">
+              Get Started
+            </Button>
+          </div>
+        </PageContainer>
+        <PageContainer>
           <StyledFooter>
-            <PageContainer>
-              <Row>
-                <Column md={3} lg={6}>
-                  <Heading>
-                    <Link to="/">packup</Link>
-                  </Heading>
-                  <p>Get outside faster and safer.</p>
-                </Column>
-                <Column sm={4} md={3} lg={2}>
-                  <p>
-                    <Link to="/" onClick={() => trackEvent('Footer Link Click', { link: 'Home' })}>
-                      Home
-                    </Link>
-                  </p>
-                  <p>
-                    <Link
-                      to="https://packupapp.com"
-                      onClick={() => trackEvent('Footer Link Click', { link: 'Sign Up' })}
-                    >
-                      Sign Up
-                    </Link>
-                  </p>
-                </Column>
-                <Column sm={4} md={3} lg={2}>
-                  <p>
-                    <Link
-                      to="/blog"
-                      onClick={() => trackEvent('Footer Link Click', { link: 'Blog' })}
-                    >
-                      Blog
-                    </Link>
-                  </p>
-                  <p>
-                    <Link
-                      to="/about"
-                      onClick={() => trackEvent('Footer Link Click', { link: 'About' })}
-                    >
-                      About
-                    </Link>
-                  </p>
-                </Column>
-                <Column sm={4} md={3} lg={2}>
-                  <p>
-                    <Link
-                      to="/contact"
-                      onClick={() => trackEvent('Footer Link Click', { link: 'Send a message' })}
-                    >
-                      Send a Message
-                    </Link>
-                  </p>
-                  <p>
-                    <a
-                      href="https://reddit.com/r/packup"
-                      onClick={() => trackEvent('Footer Link Click', { link: 'Send a message' })}
-                    >
-                      Community
-                    </a>
-                  </p>
-                  {/* <p>
-                    {colorMode ? (
-                      // eslint-disable-next-line jsx-a11y/label-has-associated-control
-                      <label>
-                        <input
-                          type="checkbox"
-                          checked={colorMode === 'dark'}
-                          onChange={(ev) => {
-                            setColorMode(ev.target.checked ? 'dark' : 'light');
-                          }}
-                        />{' '}
-                        {colorMode === 'dark' ? `🌝` : `🌞`}
-                      </label>
-                    ) : null}
-                  </p> */}
-                </Column>
-              </Row>
-              <HorizontalRule />
-              <FlexContainer justifyContent="space-between">
+            <Row>
+              <Column md={3} lg={6}>
+                <Heading>
+                  <Link to="/">packup</Link>
+                </Heading>
+                <p>Get outside faster and safer.</p>
                 <nav>
                   <Social
                     href="https://www.instagram.com/getpackup/"
@@ -249,13 +88,13 @@ const Footer = () => {
                     <HiddenText>Facebook</HiddenText>
                   </Social>
                   <Social
-                    href="https://twitter.com/getpackup"
+                    href="https://reddit.com/r/packup"
                     target="_blank"
                     rel="noopener"
-                    onClick={() => trackEvent('Footer Link Click', { link: 'Twitter' })}
+                    onClick={() => trackEvent('Footer Link Click', { link: 'Reddit' })}
                   >
-                    <FaTwitter />
-                    <HiddenText>Twitter</HiddenText>
+                    <FaReddit />
+                    <HiddenText>Reddit</HiddenText>
                   </Social>
                 </nav>
                 <small>
@@ -264,71 +103,94 @@ const Footer = () => {
                     onClick={() => trackEvent('Footer Link Click', { link: 'Privacy' })}
                   >
                     Privacy
-                  </Link>{' '}
+                  </Link>
+                  {' | '}
                   <Link
                     to="/terms"
                     onClick={() => trackEvent('Footer Link Click', { link: 'Terms of Use' })}
                   >
                     Terms of Use
-                  </Link>{' '}
+                  </Link>
+                  <br />
                   {`Copyright © Packup Technologies, Ltd. ${new Date().getFullYear()}`}
                 </small>
-              </FlexContainer>
-            </PageContainer>
+              </Column>
+              <Column sm={4} md={3} lg={2}>
+                <p>
+                  <Link to="/" onClick={() => trackEvent('Footer Link Click', { link: 'Home' })}>
+                    Home
+                  </Link>
+                </p>
+                <p>
+                  <a
+                    href="https://packupapp.com"
+                    onClick={() => trackEvent('Footer Link Click', { link: 'Sign Up' })}
+                  >
+                    Sign Up
+                  </a>
+                </p>
+              </Column>
+              <Column sm={4} md={3} lg={2}>
+                <p>
+                  <Link
+                    to="/blog"
+                    onClick={() => trackEvent('Footer Link Click', { link: 'Blog' })}
+                  >
+                    Blog
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    to="/about"
+                    onClick={() => trackEvent('Footer Link Click', { link: 'About' })}
+                  >
+                    About
+                  </Link>
+                </p>
+              </Column>
+              <Column sm={4} md={3} lg={2}>
+                <p>
+                  <Link
+                    to="/contact"
+                    onClick={() => trackEvent('Footer Link Click', { link: 'Send a message' })}
+                  >
+                    Send a Message
+                  </Link>
+                </p>
+                <p>
+                  <a
+                    href="https://reddit.com/r/packup"
+                    onClick={() => trackEvent('Footer Link Click', { link: 'Reddit' })}
+                  >
+                    Community
+                  </a>
+                </p>
+              </Column>
+            </Row>
+            <div style={{ marginTop: tripleSpacer }}>
+              <Badges />
+            </div>
           </StyledFooter>
-        </>
-      )}
-      {size.isSmallScreen && loggedInUser && !isInOnboardingFlow && (
-        <BottomNav>
-          <Link
-            to="/app/trips"
-            getProps={isPartiallyActive}
-            onClick={() =>
-              trackEvent('Logged In Small Screen Footer Link Click', { link: 'Trips' })
-            }
-          >
-            <FaCalendar />
-            {pendingTrips.length > 0 && <NotificationDot top={`-${halfSpacer}`} right="0" />}
-          </Link>
-          <Link
-            to="/app/gear-closet"
-            getProps={isPartiallyActive}
-            onClick={() =>
-              trackEvent('Logged In Small Screen Footer Link Click', { link: 'Gear Closet' })
-            }
-          >
-            <GearClosetIcon size={15} />
-          </Link>
-          {/* TODO: when shopping list is ready <Link
-            to="/app/shopping-list"
-            getProps={isPartiallyActive}
-            onClick={() =>
-              trackEvent('Logged In Small Screen Footer Link Click', { link: 'Shopping List' })
-            }
-          >
-            <FaShoppingCart />
-          </Link> */}
-          {profile.isAdmin && (
-            <Link to="/admin/gear-list" getProps={isPartiallyActive}>
-              <FaUserLock />
-            </Link>
-          )}
-          <Link
-            to="/app/profile"
-            getProps={isPartiallyActive}
-            onClick={() =>
-              trackEvent('Logged In Small Screen Footer Link Click', { link: 'Profile' })
-            }
-          >
-            <Avatar
-              src={profile.photoURL as string}
-              size="xs"
-              gravatarEmail={profile.email as string}
-            />
-          </Link>
-        </BottomNav>
-      )}
-    </>
+        </PageContainer>
+      </FlexContainer>
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          backgroundImage: `url(${mountainScene})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: '100%',
+          backgroundPosition: 'bottom center',
+          padding: halfSpacer,
+          display: 'flex',
+          alignItems: 'flex-end',
+          height: '100vh',
+          zIndex: 0,
+        }}
+      />
+    </Section>
   );
 };
 export default Footer;
--- src/redux/ducks/index.ts
+++ src/redux/ducks/index.ts
@@ -2,21 +2,15 @@ import client from '@redux/ducks/client';
 import { ClientStoreType } from '@redux/ducks/client.d';
 import globalAlerts from '@redux/ducks/globalAlerts';
 import { GlobalAlertsStoreType } from '@redux/ducks/globalAlerts.d';
-import { FirebaseReducer, firebaseReducer } from 'react-redux-firebase';
 import { combineReducers } from 'redux';
-import { firestoreReducer } from 'redux-firestore';
 
 export type RootState = {
-  firebase: FirebaseReducer.Reducer<any, any>;
-  firestore: any;
   client: ClientStoreType;
   globalAlerts: GlobalAlertsStoreType;
 };
 
 const rootReducer = combineReducers<RootState>({
   client,
-  firebase: firebaseReducer,
-  firestore: firestoreReducer,
   globalAlerts,
 });
 
